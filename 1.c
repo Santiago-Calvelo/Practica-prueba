d5a757e07f9773f1800196e35c62b59b45ab7719
Santiago-Calvelo <102254693+Santiago-Calvelo@users.noreply.github.com>
2022-05-25 20:41:37 -0300

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]) {
+  int n = atoi(argv[1]);
+  int fib[n];
+  fib[0] = 0;
+  fib[1] = 1;
+  printf("%d ", fib[0]);
+  printf("%d ", fib[1]);
+
+  for (int i = 2; i < n; i++) {
+    fib[i] = fib[i-1] + fib[i-2];
+    printf("%d ", fib[i]);
+  }
+  printf("\n");
+}
