d71baf42490061f08b5113499c022c2738ef9f5a
Santiago-Calvelo <102254693+Santiago-Calvelo@users.noreply.github.com>
2022-05-26 08:44:53 -0300

    Update 34.c

--- 34.c
+++ 34.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 int main(int argc, char const *argv[]) {
-  int n = atoi(argv[0]);
-  bool primo = true;
+  int n = atoi(argv[1]);
   for (int i = 0; i < n; i++) {
-    if (n % i == 0) primo = false;
-    if (primo == true) printf("El numero es primo\n");
-    else               printf("El numero no es primo\n");
+    if (n % i == 0) { 
+      printf("El numero no es primo\n");
+      return 1;
+     }
+    else {             
+      printf("El numero es primo\n");
+      return 1;
+     }
   }
 
   return 0;
